7e5f29ca3cf4a1604091ff467437d3bbe202bc3f
wangkun <wangkun@ablecloud.cn>
2017-03-31 20:12:33 +0800

    Init commit for refactor
    Signed-off-by: wangkun <wangkun@ablecloud.cn>

---
+++ app/src/main/java/ablecloud/matrix/app/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package ablecloud.matrix.app.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wangkun on 31/03/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
